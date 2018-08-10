192e4eef9c5483354db58d38354593557eeaca3e
nimbus117 <mail@nimbus117.co.uk>
2018-08-10 12:02:38 +0100

    added guest has tests

---
+++ specs/guest_spec.rb
@@ -0,0 +1,26 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../guest')
+
+class TestGuest < MiniTest::Test
+
+  def setup
+    @song1 = Song.new('Remedy', 'My Baby')
+    @song2 = Song.new('One', 'Metallica')
+    @songs = [@song1, @song2]
+    @guest1 = Guest.new('Bob', 20, @song1)
+    @guest2 = Guest.new('Bob', 30, @song2)
+  end
+
+  def test_guest_has_name
+    assert_equal('Bob', @guest1.name)
+  end
+
+  def test_guest_has_wallet
+    assert_equal(30, @guest2.wallet)
+  end
+
+  def test_guest_has_fav_song
+    assert_equal(@song1, @guest1.fav_song)
+  end
+end
