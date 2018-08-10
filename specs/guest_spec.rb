684139355c2ee5bbc6f2e26a4aa9fe235662caf8
nimbus117 <mail@nimbus117.co.uk>
2018-08-10 14:15:18 +0100

    added guest_can_check_for_fav_song tests

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -8,9 +8,10 @@ class TestGuest < MiniTest::Test
   def setup
     @song1 = Song.new('Remedy', 'My Baby')
     @song2 = Song.new('One', 'Metallica')
+    @song3 = Song.new('Atomic', 'Blondie')
     @songs = [@song1, @song2]
     @guest1 = Guest.new('Bob', 20, @song1)
-    @guest2 = Guest.new('Bob', 30, @song2)
+    @guest2 = Guest.new('Bob', 30, @song3)
   end
 
   def test_guest_has_name
@@ -27,5 +28,13 @@ class TestGuest < MiniTest::Test
 
   def test_guest_can_remove_money_from_wallet
     assert_equal(17, @guest1.remove_money(3))
+  end
+
+  def test_guest_can_check_for_fav_song__present
+    assert_equal('Whoo!', @guest1.check_for_fav_song(@songs))
+  end
+
+  def test_guest_can_check_for_fav_song__not_present
+    assert_nil(@guest2.check_for_fav_song(@songs))
   end
 end
