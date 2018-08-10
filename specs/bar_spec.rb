2ef3f706c535a21b5dc9e01d40a2d2a3603be212
nimbus117 <mail@nimbus117.co.uk>
2018-08-10 21:07:40 +0100

    added bar_has_empty_till test

---
+++ specs/bar_spec.rb
@@ -0,0 +1,23 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../drink')
+require_relative('../bar')
+require_relative('../till')
+
+class TestBar < MiniTest::Test
+
+  def setup
+    drink1 = Drink.new('Bud', 'Beer', 3)
+    drink2 = Drink.new('Coors', 'Beer', 3)
+    drink3 = Drink.new('Bowmore', 'Whisky', 5)
+    drink4 = Drink.new('Glenmorangie', 'Whisky', 5)
+    @drinks = [drink1, drink2, drink3, drink4]
+    till = Till.new('bar-room1')
+    @bar = Bar.new(till)
+  end
+
+  def test_bar_has_empty_till
+    assert_equal(0, @bar.till.money)
+  end
+end
+
