ffebd5a88dcefa3f6c892cde660dca7da001120f
nimbus117 <mail@nimbus117.co.uk>
2018-08-11 21:49:15 +0100

    passed karaoke_bar_can_check_in_guest test

--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -5,5 +5,13 @@ class KaraokeBar
     @name = name
     @rooms = rooms
     @till = till
+    @price = 10
+  end
+
+  def check_in_guest(room, guest)
+    payment = @till.take_payment(guest, @price)
+    if payment
+      room.push(guest)
+    end
   end
 end
