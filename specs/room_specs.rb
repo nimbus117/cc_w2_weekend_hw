416615549ea9a0201b82196e11bd12342834d6ce
nimbus117 <mail@nimbus117.co.uk>
2018-08-11 20:15:45 +0100

    added can_push_guests_to_room test

--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -1,14 +1,25 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room')
+require_relative('../guests')
+require_relative('../song')
 
 class TestRoom < MiniTest::Test
 
   def setup
     @room = Room.new()
+    @song1 = Song.new('Remedy', 'My Baby')
+    @guest1 = Guest.new('Bob', 20, @song1)
+    @guest2 = Guest.new('Sally', 20, @song1)
   end
 
   def test_room_has_capacity
     assert_equal(6, @room.capacity)
+  end
+
+  def test_can_push_guests_to_room
+    @room.guests.push(@guest1)
+    @room.guests.push(@guest2)
+    assert_equal(2, @room.guests)
   end
 end
