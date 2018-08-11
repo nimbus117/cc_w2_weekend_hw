a7c6156b20ac18a616bffb2cf0f9fcb658af1cad
nimbus117 <mail@nimbus117.co.uk>
2018-08-11 22:01:28 +0100

    added karaoke_bar_can_check_in_guest__room_at_capacity test

--- specs/karaoke_bar_spec.rb
+++ specs/karaoke_bar_spec.rb
@@ -21,6 +21,12 @@ class TestKaraokeBar < MiniTest::Test
     @song1 = Song.new('Remedy', 'My Baby')
     @poor_guest = Guest.new('Tim', 5, @song1)
     @guest1 = Guest.new('Bob', 20, @song1)
+    @guest2 = Guest.new('Ben', 20, @song1)
+    @guest3 = Guest.new('Barry', 20, @song1)
+    @guest4 = Guest.new('Babs', 20, @song1)
+    @guest5 = Guest.new('Betty', 20, @song1)
+    @guest6 = Guest.new('Bernadette', 20, @song1)
+    @guest7 = Guest.new('Frank', 20, @song1)
   end
 
   def test_karaoke_bar_has_name
@@ -48,5 +54,17 @@ class TestKaraokeBar < MiniTest::Test
     assert_equal(0, @karaoke_bar.till.money)
     assert_equal(5, @poor_guest.wallet)
     assert_equal(0, @karaoke_bar.rooms[0].guests.length)
+  end
+
+  def test_karaoke_bar_can_check_in_guest__room_at_capacity
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest1)
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest2)
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest3)
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest4)
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest5)
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest6)
+    @karaoke_bar.check_in_guest(@karaoke_bar.rooms[0].guests, @guest7)
+    assert_equal(6, @karaoke_bar.rooms[0].guests.length)
+    assert_equal(60, @karaoke_bar.till.money)
   end
 end
