d3f4b9136d64f676b48f2ef806b082503d8bd6c2
nimbus117 <mail@nimbus117.co.uk>
2018-08-11 21:17:54 +0100

    added karaoke_bar_has_empty_rooms test

--- specs/karaoke_bar_spec.rb
+++ specs/karaoke_bar_spec.rb
@@ -1,13 +1,28 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../karaoke_bar.rb')
+require_relative('../room')
+require_relative('../bar')
+require_relative('../till')
 
 class TestKaraokeBar < MiniTest::Test
   def setup
-    @karaoke_bar = KaraokeBar.new('CodeClan Caraoke')
+    till1 = Till.new('bar-room1')
+    bar1 = Bar.new(till1)
+    room1 = Room.new(bar1)
+    till2 = Till.new('bar-room2')
+    bar2 = Bar.new(till2)
+    room2 = Room.new(bar2)
+    rooms = [room1, room2]
+    @karaoke_bar = KaraokeBar.new('CodeClan Caraoke', rooms)
   end
 
   def test_karaoke_bar_has_name
     assert_equal('CodeClan Caraoke', @karaoke_bar.name)
+  end
+
+  def test_karaoke_bar_has_empty_rooms
+    assert_equal(0, @karaoke_bar.rooms[0].guests.length)
+    assert_equal(0, @karaoke_bar.rooms[1].guests.length)
   end
 end
