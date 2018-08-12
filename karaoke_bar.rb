3b44fa247b23d42f3830bc5324732a0a121b7e6e
nimbus117 <mail@nimbus117.co.uk>
2018-08-12 11:03:41 +0100

    passed karaoke_bar_get_till_report test

--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -28,4 +28,22 @@ class KaraokeBar
   def add_drinks_to_all_bars(drinks)
     @rooms.each {|room| room.bar.drinks.concat(drinks)}
   end
+
+  def get_till_report()
+    # empty array to hold report hash from each till
+    report = []
+    # array to hold each till instance, start with KaraokeBar till
+    all_tills = [@till]
+    # loop through each room and add till to all_tills array
+    @rooms.each {|room| all_tills.push(room.bar.till)}
+    # loop through each till and push id/money hash to report array
+    all_tills.each {|till| report.push({id: till.id, money: till.money})}
+    # use reduce on the all_tills array to get total money in all tills
+    total = all_tills.reduce(0) {|total, till| total + till.money}
+    # push total hash to report array
+    report.push({id: 'total', money: total})
+    # return report
+    p report
+    return report
+  end
 end
